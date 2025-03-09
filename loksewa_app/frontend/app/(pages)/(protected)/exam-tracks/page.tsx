eab233cab18318bae6afc1217efd50a9fdc339e4
Ramtamang-2058 <ram.tamang2058@gmail.com>
2025-03-10 00:10:54 +0545

    file chnages empty:

---
+++ loksewa_app/frontend/app/(pages)/(protected)/exam-tracks/page.tsx
@@ -0,0 +1,14 @@
+// exam-tracks/page.tsx
+
+import React from 'react';
+
+const ExamTracks = () => {
+  return (
+    <div>
+      <h1>Exam Tracks</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default ExamTracks;
---
+++ loksewa_app/frontend/app/(pages)/homescreen/page.tsx
@@ -0,0 +1,14 @@
+// black homescreens/page.tsx
+
+import React from 'react';
+
+const BlackHomescreens = () => {
+  return (
+    <div>
+      <h1>Black Homescreens</h1>
+      {/* Add your content here */}
+    </div>
+  );
+};
+
+export default BlackHomescreens;
