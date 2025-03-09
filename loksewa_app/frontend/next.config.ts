637f0ecc2c84c3565e88ffadfe31e35a8734fd8e
Ramtamang-2058 <ram.tamang2058@gmail.com>
2025-03-09 23:26:41 +0545

    loksewa aplication deploy

--- loksewa_app/frontend/next.config.ts
+++ loksewa_app/frontend/next.config.ts
@@ -1,10 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  assetPrefix: isProd ? '/projectpragati/' : '', // Replace 'projectpragati' with your repo name
+  output: 'export', // Export static HTML files for GitHub Pages
+  trailingSlash: true, // Ensures all links work properly on GitHub Pages
   images: {
-    domains: ['images.unsplash.com'],
+    unoptimized: true, // GitHub Pages does not support Next.js image optimization
   },
-  /* config options here */
 };
 
 export default nextConfig;
