beb417c8ffb88a70ab4e211866f2228632804071
Ramtamang-2058 <ram.tamang2058@gmail.com>
2025-03-10 00:04:48 +0545

    reve

---
+++ loksewa_app/frontend/app/(pages)/(protected)/course-material/page.tsx
@@ -0,0 +1,14 @@
+// /app/(pages)/(protected)/course-material/page.tsx
+
+import React from 'react';
+
+const CourseMaterialPage = () => {
+  return (
+    <div>
+      <h1>Course Material</h1>
+      {/* Add your course material content here */}
+    </div>
+  );
+};
+
+export default CourseMaterialPage;
