9fb327f0e46a29b5d59b95079b38b19e993c9698
Ramtamang-2058 <ram.tamang2058@gmail.com>
2025-05-13 22:41:42 +0545

    Added workflow

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,1 +1,12 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Required for static export
+  },
+  // Ensures compatibility with GitHub Pages
+  basePath: process.env.NODE_ENV === 'production' ? '/projectpragati' : '',
+};
+
+module.exports = nextConfig;
